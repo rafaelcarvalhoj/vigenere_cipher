10305c694ff59b8c04939bedb92046081d9a88ae
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2026-04-02 20:25:26 -0300

    feat: vigenere skeleton

---
+++ vigenere_cipher.py
@@ -0,0 +1,12 @@
+
+
+
+class VigenereCipher:
+    
+    @staticmethod
+    def encrypt(self, key: str) -> str:
+        pass
+
+    @staticmethod
+    def decryrpt(self, key: str) -> str:
+        pass
